17ec4777a5599a7b1d57421d6ccdc1e9d28d402a
alanwales223 <105378608+alanwales223@users.noreply.github.com>
2022-05-20 01:57:29 -0700

    Update FFC.sol

--- FFC.sol
+++ FFC.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GLDToken is ERC20 {
+contract FistFarmCoin is ERC20 {
     constructor(uint256 initialSupply) ERC20("FistFarmCoin", "FFC") {
         _mint(msg.sender, initialSupply);
     }
